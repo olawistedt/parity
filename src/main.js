98071de47ac8717277691726e7e824abbc8065b4
Ola Wistedt <ola@witech.se>
2021-01-22 18:06:32 +0100

    Hard coded height and width.

--- src/main.js
+++ src/main.js
@@ -9,9 +9,13 @@ const CST = {
 }
 
 let game = new Phaser.Game({
-    width: window.innerWidth * 1.00,
-    height: window.innerHeight * 1.00,
-  scale: {mode: Phaser.Scale.HEIGHT_CONTROLS_WIDTH},
+//  width: 1536,  // Ipad 4 width
+//  height: 2048, // Ipad 4 height
+  width: 1536 * 0.55,
+  height: 2048 * 0.55,
+//  width: window.innerWidth * 1.00,
+//  height: window.innerHeight * 1.00,
+//  scale: {mode: Phaser.Scale.HEIGHT_CONTROLS_WIDTH},
   audio: {disableWebAudio: true},
 
   scene: [
